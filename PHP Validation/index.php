291f1e0ced26b0ca06724fc9b213b0f80b15141c
Gokul Krishna jayan <91828907+Gokulkrishnajayan@users.noreply.github.com>
2023-12-20 12:32:36 +0530

    Update index.php

--- PHP Validation/index.php
+++ PHP Validation/index.php
@@ -4,8 +4,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Validation</title>
 </head>
+<?php
+ $name='';
+ $email='';
+ $phone='';
+ $password='';
+ $cpassword='';
+ //  $gender='';
+ $address='';
+ $pemail="/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/";
+ $pphone="/^\d{10}$/";
+ $ppassword="/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/";
+if (isset($_POST['submit'])){
+    $con=mysqli_connect('localhost','root','','mca');
+    if(!$con){
+        echo("Connection failed!");
+    }else{
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $phone = mysqli_real_escape_string($con, $_POST['phone']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+    $cpassword = mysqli_real_escape_string($con, $_POST['cpassword']);
+    // $gender = mysqli_real_escape_string($con, $_POST['gender']);
+    $address = mysqli_real_escape_string($con, $_POST['address']);
+        if(empty($name)){
+            echo "<script>alert('Name field cannot be empty')</script>";
+        }  
+        elseif(empty($email)){
+            echo "<script>alert('Gmail field cannot be empty')</script>";
+        }
+        elseif(empty($phone)){
+            echo "<script>alert('Phone field cannot be empty')</script>";
+        }
+        elseif(empty($password)&&empty($cpassword)){
+            echo "<script>alert('Password field Cannot be empty')</script>";
+        //}
+        // elseif(empty($gender)){
+        //     echo "<script>alert('Gender field connot be empty')</script>";
+        }
+        elseif(empty($address)){
+            echo "<script>alert('Address field connot be empty')</script>";
+        }
+        elseif(!preg_match($pemail,$email)) {
+            echo "<script>alert('Email Not valid')</script>";
+        }
+        elseif(!preg_match($pphone,$phone)) {
+            echo "<script>alert('Phone number Not valid')</script>";
+        }
+        elseif(!preg_match($ppassword,$password)) {
+            if ($password!=$cpassword) {
+                echo "<script>alert('Password not Matching')</script>";
+            }
+            else {
+                echo "<script>alert('Password not valid')</script>";
+            }   
+        }   
+    }    
+}
+?>
 
 <body>
     <form action="#" method="POST">
@@ -16,7 +74,7 @@
                     Name
                 </td>
                 <td>
-                    :<input type="text" name="name">
+                    :<input type="text" name="name" value="<?php echo $name;?>">
                 </td>
             </tr>
             <tr>
@@ -24,7 +82,7 @@
                     Email
                 </td>
                 <td>
-                    :<input type="email" name="email">
+                    :<input type="email" name="email" value="<?php echo $email;?>">
                 </td>
             </tr>
             <tr>
@@ -32,7 +90,7 @@
                     Phone
                 </td>
                 <td>
-                    :<input type="text" name="phone">
+                    :<input type="text" name="phone" value="<?php echo $phone;?>">
                 </td>
             </tr>
             <tr>
@@ -40,7 +98,7 @@
                     Password
                 </td>
                 <td>
-                    :<input type="password" name="password">
+                    :<input type="password" name="password" value="<?php echo $password;?>">
                 </td>
             </tr>
             <tr>
@@ -48,7 +106,7 @@
                     Confirm Password
                 </td>
                 <td>
-                    :<input type="password" name="cpassword">
+                    :<input type="password" name="cpassword" value="<?php echo $cpassword;?>">
                 </td>
             </tr>
             <tr>
@@ -65,7 +123,7 @@
                     Address
                 </td>
                 <td>
-                    :<textarea name="address"></textarea>
+                    :<textarea name="address" value="<?php echo $address;?>"></textarea>
                 </td>
             </tr>
             <tr>
@@ -73,59 +131,10 @@
                     <button type="submit" name="submit">Submit</button>
                     <button type="reset">Reset</button>
                 </td>
-
             </tr>
-
         </table>
     </form>
 
 </body>
-<?php
- $name='';
- $gmail='';
- $phone='';
- $password='';
- $cpassword='';
-//  $gender='';
- $address='';
-if (isset($_POST['submit'])){
-    $con=Mysqli_Connect('localhost','root','','mca');
-    if(!$con){
-        echo("Connection failed!");
-    }else{
-    $name = mysqli_real_escape_string($con, $_POST['name']);
-    $gmail = mysqli_real_escape_string($con, $_POST['email']);
-    $phone = mysqli_real_escape_string($con, $_POST['phone']);
-    $password = mysqli_real_escape_string($con, $_POST['password']);
-    $cpassword = mysqli_real_escape_string($con, $_POST['cpassword']);
-    // $gender = mysqli_real_escape_string($con, $_POST['gender']);
-    $address = mysqli_real_escape_string($con, $_POST['address']);
-        if(empty($name)){
-            echo "<script>alert('Name field cannot be empty')</script>";
-        }   
-        elseif(empty($gmail)){
-            echo "<script>alert('Gmail field cannot be empty')</script>";
-        }
-        elseif(empty($phone)){
-            echo "<script>alert('Phone field cannot be empty')</script>";
-        }
-        elseif(empty($password)&&empty($cpassword)){
-            echo "<script>alert('Password field Cannot be empty')</script>";
-        //}
-        // elseif(empty($gender)){
-        //     echo "<script>alert('Gender field connot be empty')</script>";
-        }
-        elseif(empty($address)){
-            echo "<script>alert('Address field connot be empty')</script>";
-        }
-        
-    }
-
-    
-}
-
-
-
-?>
 
 </html>
